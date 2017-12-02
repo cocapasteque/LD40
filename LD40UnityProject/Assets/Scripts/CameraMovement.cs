b11690c61e99f5e8f650038f7bbfacee12202578
Antoine Thiry <antoine@303.media>
2017-12-02 12:55:09 +0100

    Doors + camera scripts

---
+++ LD40UnityProject/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public static CameraMovement instance;
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != null) Destroy(this);
+    }
+
+    public void MoveLeft()
+    {
+        Debug.Log("Move left");
+    }
+
+    public void MoveRight()
+    {
+        Debug.Log("Move right");
+    }
+
+    public void MoveUp()
+    {
+        Debug.Log("Move up");
+    }
+    public void MoveDown()
+    {
+        Debug.Log("Move down");
+
+    }
+}
