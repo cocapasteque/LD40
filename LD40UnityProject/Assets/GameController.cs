e1189b853d4d9a8ea8c6618aa0d48a59bac22848
Antoine Thiry <antoine@303.media>
2017-12-02 14:14:47 +0100

    Map creation + camera

--- LD40UnityProject/Assets/GameController.cs
+++ LD40UnityProject/Assets/GameController.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public static GameController instance;
 
-    public Room[] map;
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject roomPrefab;
+
+    public int width = 5;
+    public int height = 5;
+    public int offset = 10;
+    public Room[,] map;
+    public List<GameObject> rooms;
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(this);
+    }
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    void GenerateMap()
+    {
+        map = new Room[width, height];
+        var mapObject = GameObject.Find("Map");
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                var go = Instantiate(roomPrefab);
+                go.transform.parent = mapObject.transform;
+                go.transform.position = new Vector3(i * offset, j * offset);
+                map[i, j] = go.GetComponent<Room>();
+                rooms.Add(go);
+            }
+        }
+    }
 }
--- LD40UnityProject/Assets/Scripts/CameraMovement.cs
+++ LD40UnityProject/Assets/Scripts/CameraMovement.cs
@@ -10,19 +10,31 @@ public class CameraMovement : MonoBehaviour
     void Awake()
     {
         if (instance == null) instance = this;
-        else if (instance != null) Destroy(this);
+        else if (instance != this) Destroy(this);
     }
 
     public void MoveLeft()
     {
+        var offset = GameController.instance.offset;
+        var newPos = new Vector3(transform.position.x - offset, transform.position.y, transform.position.z);
+        transform.position = newPos;
     }
     public void MoveRight()
     {
+        var offset = GameController.instance.offset;
+        var newPos = new Vector3(transform.position.x + offset, transform.position.y, transform.position.z);
+        transform.position = newPos;
     }
     public void MoveUp()
     {
+        var offset = GameController.instance.offset;
+        var newPos = new Vector3(transform.position.x, transform.position.y + offset, transform.position.z);
+        transform.position = newPos;
     }
     public void MoveDown()
     {
+        var offset = GameController.instance.offset;
+        var newPos = new Vector3(transform.position.x, transform.position.y - offset, transform.position.z);
+        transform.position = newPos;
     }
 }
