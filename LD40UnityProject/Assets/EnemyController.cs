02926316b7b22fe5fc6b7551fc373d1d9af7615c
Antoine Thiry <antoine@303.media>
2017-12-02 18:47:32 +0100

    Ranged behaviour

--- LD40UnityProject/Assets/EnemyController.cs
+++ LD40UnityProject/Assets/EnemyController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public GameObject bulletPrefab;
     public EnemyType type;
     public float speed = 2f;
     private Transform player;
@@ -34,6 +35,17 @@ public class EnemyController : MonoBehaviour
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         dir.Normalize();
         GetComponent<Rigidbody2D>().MovePosition(transform.position + dir * Time.deltaTime * speed);
+    }
+
+    void RangedBehaviour()
+    {
+        var dir = player.position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        dir.Normalize();
+
+        var clone = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        clone.GetComponent<Rigidbody2D>().velocity = dir * clone.GetComponent<Projectile>().spell.Speed;
     }
 
     void OnTriggerEnter2D(Collider2D other)
