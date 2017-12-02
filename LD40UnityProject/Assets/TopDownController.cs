a529144d30030b41aa6840ceb699601366c2313e
Antoine Thiry <antoine@303.media>
2017-12-02 05:57:41 +0100

    First commit

---
+++ LD40UnityProject/Assets/TopDownController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class TopDownController : MonoBehaviour
+{
+    #region Public properties
+    public float moveSpeed = 5f;
+    #endregion
+
+    #region Private properties
+    private Rigidbody2D rb;
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var h = Input.GetAxis("Horizontal");
+	    var v = Input.GetAxis("Vertical");
+        Move(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        var move = new Vector2(transform.position.x + (h * Time.deltaTime * moveSpeed),transform.position.y + (v * Time.deltaTime * moveSpeed));
+        rb.MovePosition(move);
+    }
+}
---
+++ ideas.md
@@ -0,0 +1,8 @@
+# Ludum Dare 40 - The more you have, the worse it is.
+
+- Weeds in the garden. You are an old retired man who try to keep his garden clean by cuting the weed. The more you have, the less happy you are.
+
+- 
+
+
+
