4a3feb1e02eb15b6c91733d05d475561b1258473
Kurotoutou <fuuton22@gmail.com>
2017-12-02 22:12:17 +0100

    Fix topdownctrl

--- LD40UnityProject/Assets/Scripts/TopDownController.cs
+++ LD40UnityProject/Assets/Scripts/TopDownController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
@@ -15,23 +16,32 @@ public class TopDownController : MonoBehaviour
     private Rigidbody2D rb;
     #endregion
     public GameObject projectile;
+    public GameObject combatText;
+
     public GameObject shootPosition;
-    // Use this for initialization
-    void Start ()
+
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0;
     }
-	
-	// Update is called once per frame
-	void Update ()
-	{
+
+    // Update is called once per frame
+    void Update()
+    {
         var h = Input.GetAxis("Horizontal");
-	    var v = Input.GetAxis("Vertical");
+        var v = Input.GetAxis("Vertical");
         Move(h, v);
         Attack();
-	    RegenSanity();
-	}
+
+        var canvas = transform.Find("Canvas");
+        canvas.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
+        canvas.rotation = Quaternion.identity;
+
+    }
+
+
+
 
     void FixedUpdate()
     {
@@ -43,15 +53,20 @@ public class TopDownController : MonoBehaviour
 
     void Move(float h, float v)
     {
-        var move = new Vector2(transform.position.x + (h * Time.deltaTime * moveSpeed),transform.position.y + (v * Time.deltaTime * moveSpeed));
+        var move = new Vector2(transform.position.x + (h * Time.deltaTime * moveSpeed), transform.position.y + (v * Time.deltaTime * moveSpeed));
         rb.MovePosition(move);
     }
 
     void Attack()
     {
+        if (Input.GetButton("Fire2"))
+        {
+            if (insanity > 0) insanity -= 0.5f;
+            return;
+        }
         if (Input.GetButtonDown("Fire1"))
         {
-            insanity += 5;
+            if (insanity < 100) insanity += 5;
             var clone = Instantiate(projectile, shootPosition.transform.position, transform.rotation);
 
             var mousePos =
@@ -63,18 +78,16 @@ public class TopDownController : MonoBehaviour
         }
     }
 
-    void RegenSanity()
-    {
-        if (Input.GetButton("Fire2"))
-        {
-            
-        }
-    }
 
-    public void Hit(float damage)
+    public void Hit(float damageReceived)
     {
-        health -= damage;
+        health -= damageReceived;
         insanity += 5;
+        var canvas = transform.Find("Canvas");
+        var cbtxt = Instantiate(combatText, canvas.position, canvas.rotation, canvas);
+        cbtxt.GetComponent<Text>().text = ((int)damageReceived).ToString();
+        Debug.Log((int)damageReceived);
+        //Debug.Log("AIE MORRAY");
     }
 
 }
